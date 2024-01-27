4b9668d9d8baa2c28490e06b520c0213c1fe4588
Nikolay <Novizdarialokin@yandex.ru>
2024-01-27 17:55:58 +0600

    feat: Добавил класс Фильм

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,37 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.Instant;
+
 /**
  * Film.
+ *
+ * @author Nikolay Radzivon
  */
-@Getter
-@Setter
+@Data
 public class Film {
+    /**
+     * Индификатор фильма
+     */
+    private final int id;
+    /**
+     * Имя фильма
+     */
+    private final String name;
+    /**
+     * Описание фильма
+     */
+    private final String description;
+    /**
+     * Дата релиза фильма
+     */
+    private final Instant releaseDate;
+    /**
+     * Продолжительнность фильма
+     */
+    private final int duration;
+
 }
