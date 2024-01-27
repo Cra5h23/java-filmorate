cc4b0a957d2087a0680be1478f344e126da63767
Nikolay <Novizdarialokin@yandex.ru>
2024-01-27 19:47:38 +0600

    feat: Добавил класс FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,8 +1,22 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exeptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.Instant;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Класс FilmController
+ *
+ * @author Nikolay Radzivon
+ */
 @RestController
+@RequestMapping("/films")
 public class FilmController {
     private final Map<Integer, Film> films = new HashMap<>();
 
@@ -19,9 +33,23 @@ public class FilmController {
     }
 
 
-    private Film filmValidator (Film film) {
-        if (film.getName().isBlank() || film.getName() == null) {
+    private void filmValidator(Film film) {
+        int validationDescriptionLength = 200;
+        Instant validationReleaseDate = Instant.parse("1985-12-28T");
 
+        if (film.getName().isBlank() || film.getName() == null) {
+            throw new ValidationException("Имя фильма не должно быть пустым");
+        }
+        if (film.getDescription().length() > validationDescriptionLength) {
+            throw new ValidationException("Длинна описания не должна привышать 200 символов");
+        }
+
+        if (film.getReleaseDate().isBefore(validationReleaseDate)) {
+            throw new ValidationException("Дата релиза не должна быть раньше " + validationReleaseDate);
+        }
+
+        if (film.getDuration() > 0) {
+            throw new ValidationException("Продолжительность фильма должна быть положительной");
         }
     }
 }
