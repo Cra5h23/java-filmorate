c2910c9a9709fe208e4f5cce1acbbafe94369e95
Nikolay <Novizdarialokin@yandex.ru>
2024-01-27 18:23:36 +0600

    feat: Добавил класс FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -4,4 +4,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    private final int generatorId = 0;
+
+    @GetMapping
+    public Collection getAllFilms() {
+        return Collections.unmodifiableCollection(films.values());
+    }
+
+    @PostMapping("/film")
+    public Film addFilm(@RequestBody Film film) {
+        return film;
+    }
+
+
+    private Film filmValidator (Film film) {
+        if (film.getName().isBlank() || film.getName() == null) {
+
+        }
+    }
 }
