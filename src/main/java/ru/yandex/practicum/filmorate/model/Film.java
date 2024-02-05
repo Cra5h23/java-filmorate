634b12924509d64382adc90204995c4d5eeeb5f2
Nikolay <Novizdarialokin@yandex.ru>
2024-02-05 15:50:35 +0600

    feat: добавил валидацию с помощью анотаций

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,9 +1,18 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Value;
+import ru.yandex.practicum.filmorate.model.validator.MinReleaseData;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 /**
@@ -12,7 +21,7 @@ import java.time.LocalDate;
  * @author Nikolay Radzivon
  */
 @Data
-@Builder
+@Builder(toBuilder = true)
 public class Film {
     /**
      * Индификатор фильма
@@ -23,17 +32,29 @@ public class Film {
     /**
      * Имя фильма
      */
+    @NotBlank(message = "Название фильма не должно быть пустым")
+    @NotNull
     private String name;
+
     /**
      * Описание фильма
      */
+    @Size(max = 200, message = "Описание фильма не должно быть больше {max}")
     private String description;
+
     /**
      * Дата релиза фильма
      */
+    @NotNull
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @MinReleaseData("1895-12-28")
     private LocalDate releaseDate;
+
     /**
      * Продолжительнность фильма
      */
+    @Positive(message = "Продолжительность фильма должна быть положительной")
     private int duration;
 }
