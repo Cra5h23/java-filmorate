293b4146dda47e3d69265d57ef14175502ea571f
Nikolay <Novizdarialokin@yandex.ru>
2024-02-05 15:52:51 +0600

    feat: Реализация анотации для валидации даты релиза

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/validator/MinReleaseDateValidator.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.model.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+
+public class MinReleaseDateValidator implements ConstraintValidator<MinReleaseData, LocalDate> {
+    private LocalDate minData;
+
+    @Override
+    public void initialize(MinReleaseData constraintAnnotation) {
+        minData = LocalDate.parse(constraintAnnotation.value());
+    }
+
+    @Override
+    public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
+        if (value == null || !value.isBefore(minData)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
