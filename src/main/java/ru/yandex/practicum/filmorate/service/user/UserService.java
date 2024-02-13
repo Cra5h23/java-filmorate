11a7c8e3302f439f9ce57c8d950bae2c37acb47d
Nikolay <Novizdarialokin@yandex.ru>
2024-02-13 18:24:37 +0600

    feat: реализация? метода addingUserAsFriend

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -2,7 +2,14 @@ package ru.yandex.practicum.filmorate.service.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exeption.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
 
 @Service
 public class UserService {
@@ -13,7 +20,15 @@ public class UserService {
         this.userStorage = userStorage;
     }
 
-    public void addingUserAsFriend() {
+    public void addingUserAsFriend(int userId, int friendId) {
+        if (userStorage.getUserById(userId) == null) {
+            throw new ValidationException(format("Нет пользователя с id:%d",userId));
+        }
+        if (userStorage.getUserById(friendId) == null) {
+            throw new ValidationException(format("Попытка добавить в друзья несуществующего пользователя с id:%d",friendId));
+        }
+        userStorage.getUserById(userId).getFriends().add(friendId);
+    }
 
     }
 }
