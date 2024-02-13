6a3ab47f7e796845eb189a75e1deea6e8766f998
Nikolay <Novizdarialokin@yandex.ru>
2024-02-13 16:23:12 +0600

    feat: Реализация через userStorage

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,8 +3,8 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exeption.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import javax.validation.Valid;
 import java.util.Collection;
@@ -21,43 +21,54 @@ import java.util.Map;
 @Slf4j
 public class UserController {
     @Getter
-    private final Map<Integer, User> userMap = new HashMap<>();
+    private final Map<Integer, User> userMap = new HashMap<>();//todo
 
-    private int generatorUserId = 0;
+    private int generatorUserId = 0;//todo
+
+    private final UserStorage userStorage;
+
+    public UserController(UserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
 
     @GetMapping
     public Collection<User> getAllUsers() {
-        return userMap.values();
+        return userStorage.getAllUsers();
+//        return userMap.values();
     }
 
     @PostMapping
     public User addNewUser(@Valid @RequestBody User user) {
-        user.setId(++generatorUserId);
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
-        userMap.put(user.getId(), user);
-        log.info("Добавлен пользователь {}", user);
-        return user;
+        return userStorage.addUser(user);
+//        user.setId(++generatorUserId);
+//        if (user.getName() == null || user.getName().isBlank()) {
+//            user.setName(user.getLogin());
+//        }
+//        userMap.put(user.getId(), user);
+//        log.info("Добавлен пользователь {}", user);
+//        return user;
     }
 
 
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) {
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
-        int userId = user.getId();
-        User u = userMap.get(userId);
-        if (u == null) {
-            log.warn("Нет пользователя с id: {}", userId);
-            throw new ValidationException("Нет пользователя с id: " + userId);
-        }
-        u.setName(user.getName());
-        u.setLogin(user.getLogin());
-        u.setEmail(user.getEmail());
-        u.setBirthday(user.getBirthday());
-        log.info("Обновлён пользователь с id: {}", userId);
-        return u;
+        return userStorage.updateUser(user);
+//        if (user.getName() == null || user.getName().isBlank()) {
+//            user.setName(user.getLogin());
+//        }
+//        int userId = user.getId();
+//        User u = userMap.get(userId);
+//        if (u == null) {
+//            log.warn("Нет пользователя с id: {}", userId);
+//            throw new ValidationException("Нет пользователя с id: " + userId);
+//        }
+//        u.setName(user.getName());
+//        u.setLogin(user.getLogin());
+//        u.setEmail(user.getEmail());
+//        u.setBirthday(user.getBirthday());
+//        log.info("Обновлён пользователь с id: {}", userId);
+//        return u;
     }
+    /*@DeleteMapping
+    public void deleteUser(@)*/
 }
