70281059f7a5411cbca78cae10a96d105c34c2be
Nikolay <Novizdarialokin@yandex.ru>
2024-02-13 16:23:41 +0600

    feat: Добавил UserService

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.service.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+@Service
+public class UserService {
+    private final UserStorage userStorage;
+
+    @Autowired
+    public UserService(UserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
+
+    public void addingUserAsFriend() {
+
+    }
+}
