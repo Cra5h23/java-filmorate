ea140d34c795adff8deb112adc43abd8cac65c9f
Nikolay <Novizdarialokin@yandex.ru>
2024-02-13 12:41:08 +0600

    feat: Добавил Класс InMemoryFilmStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -0,0 +1,32 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+
+public class InMemoryFilmStorage implements FilmStorage {
+    @Override
+    public Collection<Film> getAllFilms() {
+        return null;
+    }
+
+    @Override
+    public Film addFilm(Film film) {
+        return null;
+    }
+
+    @Override
+    public Film updateFilm(Film film) {
+        return null;
+    }
+
+    @Override
+    public Film getFilmById(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteFilm(int id) {
+
+    }
+}
