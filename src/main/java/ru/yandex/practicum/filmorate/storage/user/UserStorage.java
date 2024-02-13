c9121adf7c79fcc297f9ddcb45896156e381c569
Nikolay <Novizdarialokin@yandex.ru>
2024-02-13 14:47:35 +0600

    feat: Добавил методы

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,4 +1,17 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+
 public interface UserStorage {
+    Collection<User> getAllUsers();
+
+    User addUser(User user);
+
+    User updateUser(User user);
+
+    User getUserById(int id);
+
+    void deleteUser(int id);
 }
