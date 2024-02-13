c42db6e58d5b9f8d2d74863a44ebcc88bc7b2882
Nikolay <Novizdarialokin@yandex.ru>
2024-02-13 12:45:07 +0600

    feat: Добавил filmMap для хранения всех фильмов

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -1,10 +1,15 @@
-package ru.yandex.practicum.filmorate.storage;
+package ru.yandex.practicum.filmorate.storage.film;
 
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
+@Component
 public class InMemoryFilmStorage implements FilmStorage {
+    private Map<Integer, Film> filmMap = new HashMap<>();
     @Override
     public Collection<Film> getAllFilms() {
         return null;
