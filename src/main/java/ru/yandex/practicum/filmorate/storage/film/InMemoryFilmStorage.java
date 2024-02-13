aef75425f94479da3433ce5fb5064c458bf96431
Nikolay <Novizdarialokin@yandex.ru>
2024-02-13 14:40:18 +0600

    feat: Добавил класс перенёс реализацию из FilmController

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exeption.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
@@ -9,30 +11,54 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@Slf4j
 public class InMemoryFilmStorage implements FilmStorage {
     private Map<Integer, Film> filmMap = new HashMap<>();
+    private int generatorFilmId = 0;
     @Override
     public Collection<Film> getAllFilms() {
-        return Collections.unmodifiableCollection(filmMap.values()); //todo должна ли быть неизменяемой
+        log.info("Запрошены все фильмы");
+        return Collections.unmodifiableCollection(filmMap.values()); //todo должна ли быть неизменяемой?
     }
 
     @Override
     public Film addFilm(Film film) {
-        return null;
+        film.setId(++generatorFilmId);
+        filmMap.put(film.getId(), film);
+        log.info("Добавлен фильм {}", film);
+        return film;
     }
 
     @Override
     public Film updateFilm(Film film) {
-        return null;
+        int filmId = film.getId();
+        Film f = filmMap.get(filmId);
+        if (f == null) {
+            log.warn("Нет фильма с id:" + filmId);
+            throw new ValidationException(String.format("Нет фильма с id:%d", filmId));
+        }
+        f.setName(film.getName());
+        f.setDescription(film.getDescription());
+        f.setReleaseDate(film.getReleaseDate());
+        f.setDuration(film.getDuration());
+        log.info("Обновлён фильм с id: {}", filmId);
+        return f;
     }
 
     @Override
     public Film getFilmById(int id) {
-        return null;
+        var film = filmMap.get(id);
+        if (film == null) {
+            throw new ValidationException(String.format("Нет фильма с id:%d",id));
+        }
+        return film;
     }
 
     @Override
     public void deleteFilm(int id) {
-
+        if (!filmMap.containsKey(id)) {
+            throw new ValidationException(String.format("Нет фильма с id:%s",id));
+        }
+        filmMap.remove(id);
     }
 }
