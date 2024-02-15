3f8d9c0fd9760f86efbcfac8ca5c922f54edb18b
Nikolay <Novizdarialokin@yandex.ru>
2024-02-15 13:51:04 +0600

    feat: Добавил метод для обработки LikeException

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,6 +1,11 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import ru.yandex.practicum.filmorate.exeption.LikeException;
+import ru.yandex.practicum.filmorate.model.ErrorResponse;
 
 /**
  * Класс ErrorHandler предназначен для отлова ошибок запросов и возвращать пользователю форматированный ответ
@@ -9,4 +14,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
  */
 @ControllerAdvice
 public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ErrorResponse handlerLikeException(final LikeException e) {
+        return new ErrorResponse("Неверно введены данные", e.getMessage());
+    }
 }
