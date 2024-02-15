4d485fae58bce73d37fbcf03a16633041fd4db54
Nikolay <Novizdarialokin@yandex.ru>
2024-02-15 13:42:05 +0600

    feat: Добавил класс ErrorHandler для обработки ошибок и предоставление ответа пользователю

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * Класс ErrorHandler предназначен для отлова ошибок запросов и возвращать пользователю форматированный ответ
+ *
+ * @author Nikolay Radzivon
+ */
+@ControllerAdvice
+public class ErrorHandler {
+}
