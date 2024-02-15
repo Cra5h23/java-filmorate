3a4cb4ad4853aff1ff4a63916fad6f45ed2bfe28
Nikolay <Novizdarialokin@yandex.ru>
2024-02-15 14:59:06 +0600

    feat: Добавлены методы handlerValidationException, handlerMethodArgumentNotValidException

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,23 +1,45 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.yandex.practicum.filmorate.exeption.LikeException;
-import ru.yandex.practicum.filmorate.model.ErrorResponse;
+import ru.yandex.practicum.filmorate.exeption.ValidationException;
+
+import java.util.Map;
 
 /**
  * Класс ErrorHandler предназначен для отлова ошибок запросов и возвращать пользователю форматированный ответ
  *
  * @author Nikolay Radzivon
  */
-@ControllerAdvice
+@ControllerAdvice("ru.yandex.practicum.filmorate")
 public class ErrorHandler {
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ErrorResponse handlerLikeException(final LikeException e) {
-        return new ErrorResponse("Неверно введены данные", e.getMessage());
+    public ResponseEntity<?> handlerLikeException(final LikeException e) {
+        return new ResponseEntity<>(
+                Map.of("Неверно введены данные", e.getMessage()),
+                HttpStatus.NOT_FOUND //todo исправить ResponseEntity
+        );
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handlerValidationException(final ValidationException e) {
+        return new ResponseEntity<>(
+                Map.of("Неверно введены данные", e.getMessage()),
+                HttpStatus.NO_CONTENT //todo проверить код и справить
+        );
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handlerMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        return ResponseEntity
+                .badRequest()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Map.of("Неверно введены данные", e.getFieldError().getDefaultMessage()));
     }
 }
