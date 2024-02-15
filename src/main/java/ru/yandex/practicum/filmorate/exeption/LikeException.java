529ac71f22f563d9635f5757f05ffa8febaf2557
Nikolay <Novizdarialokin@yandex.ru>
2024-02-15 13:26:35 +0600

    feat: Добавил отдельный LikeException для ошибок сервиса Лайков

---
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/LikeException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.exeption;
+
+public class LikeException extends RuntimeException {
+    public LikeException(String message) {
+        super(message);
+    }
+}
