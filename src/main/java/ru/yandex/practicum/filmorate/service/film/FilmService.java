aed4a610bb1b0af1e174bb8bddfb513d3045dbbe
Nikolay <Novizdarialokin@yandex.ru>
2024-02-17 17:23:01 +0600

    feat: Добавил отдельные методы checkUser checkFilm

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -1,10 +1,13 @@
 package ru.yandex.practicum.filmorate.service.film;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exeption.LikeException;
+import ru.yandex.practicum.filmorate.exeption.FilmServiceException;
+import ru.yandex.practicum.filmorate.exeption.FilmStorageException;
+import ru.yandex.practicum.filmorate.exeption.UserStorageException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
@@ -17,16 +20,10 @@ import java.util.stream.Collectors;
  */
 @Service
 @Slf4j
+@AllArgsConstructor
 public class FilmService {
     private final FilmStorage filmStorage;
     private final UserStorage userStorage;
-
-    @Autowired
-    public FilmService(FilmStorage filmStorage, UserStorage userStorage) {
-        this.filmStorage = filmStorage;
-        this.userStorage = userStorage;
-    }
-
 
     /**
      * Метод добавления лайка фильму
@@ -36,42 +33,57 @@ public class FilmService {
      * @return
      */
     public String addLikeFilm(Integer filmId, Integer userId) { //todo
-        var filmById = filmStorage.getFilmById(filmId);
-        if (filmById == null) {
-            throw new LikeException(String.format("Попытка добавить лайк фильму с несуществующим id:%d", filmId));
+        var film = checkFilm(filmId, "добавить", "фильму");
+        checkUser(userId, "добавить", "фильму");
+        if (film.getLikes().contains(userId)) {
+            throw new FilmServiceException(String.format("Пользователь с id:%d уже поставил лайк фильму с id:%d", userId, filmId));
         }
-        if (userStorage.getUserById(userId) == null) {
-            throw new LikeException(String.format("Попытка добавить лайк фильму от несуществующего пользователя id:%d", userId));
-        }
-        if (filmById.getLikes().contains(userId)) {
-            throw new LikeException(String.format("Пользователь с id:%d уже поставил лайк фильму с id:%d",userId , filmId));
-        }
-
-        filmById.addLike(userId);
-        log.info("Ползователь с id:{} поставил лайк фисльму с id:{}", userId, filmId);
+        film.addLike(userId);
+        log.info("Ползователь с id:{} поставил лайк фильму с id:{}", userId, filmId);
         return String.format("Пользователь с id:%d поставил лайк фильму с id:%d", userId, filmId);
+
+        //var filmById = filmStorage.getFilmById(filmId);
+//        if (filmById == null) {
+//            throw new FilmServiceException(String.format("Попытка добавить лайк фильму с несуществующим id:%d", filmId));
+//        }
+//        if (userStorage.getUserById(userId) == null) {
+//            throw new FilmServiceException(String.format("Попытка добавить лайк фильму от несуществующего пользователя id:%d", userId));
+//        }
+//        if (filmById.getLikes().contains(userId)) {
+//            throw new FilmServiceException(String.format("Пользователь с id:%d уже поставил лайк фильму с id:%d", userId, filmId));
+//        }
+
+
+        //filmById.addLike(userId);
+
     }
 
     /**
      * Метод удаления лайка из фильма
+     *
      * @param filmId
      * @param userId
      * @return
      */
     public String deleteLikeFilm(Integer filmId, Integer userId) {
-        var filmById = filmStorage.getFilmById(filmId);
-        if (filmById == null) {
-            throw new LikeException(String.format("Попытка удалить лайк у фильма с несуществующим id:%d", filmId));
-        }
-        if (userStorage.getUserById(userId) == null) {
-            throw new LikeException(String.format("Попытка удалить лайк у фильма от несуществующего пользователя id:%d", userId));
-        }
-        filmById.deleteLike(userId);
+        var film = checkFilm(filmId, "удалить", "у фильма");
+        checkUser(userId, "удалить", "у фильма");
+        film.deleteLike(userId);
         return String.format("Пользователь с id:%d удалил лайк у фильма с id:%d", userId, filmId);
+
+//        var filmById = filmStorage.getFilmById(filmId);
+//
+//        if (filmById == null) {
+//            throw new FilmServiceException(String.format("Попытка удалить лайк у фильма с несуществующим id:%d", filmId));
+//        }
+//        if (userStorage.getUserById(userId) == null) {
+//            throw new FilmServiceException(String.format("Попытка удалить лайк у фильма от несуществующего пользователя id:%d", userId));
+//        }
+//        filmById.deleteLike(userId);
+
     }
 
     /**
-     *
      * @param count
      * @return
      */
@@ -81,5 +93,23 @@ public class FilmService {
                 .sorted(Comparator.comparingInt(f -> (f.getLikes().size()) * -1))
                 .limit(count)
                 .collect(Collectors.toList());
+    }
+
+    private Film checkFilm(Integer filmId, String... s) {
+        try {
+            return filmStorage.getFilmById(filmId);
+        } catch (FilmStorageException e) {
+            //throw new FilmServiceException(String.format("Попытка добавить лайк фильму с несуществующим id:%d", filmId));
+            throw new FilmServiceException(String.format("Попытка %s лайк %s с несуществующим id:%d", s[0], s[1], filmId));
+        }
+    }
+
+    private User checkUser(Integer userId, String... s) {
+        try {
+            return userStorage.getUserById(userId);
+        } catch (UserStorageException e) {
+            //throw new FilmServiceException(String.format("Попытка добавить лайк фильму от несуществующего пользователя id:%d", userId));
+            throw new FilmServiceException(String.format("Попытка %s лайк %s от несуществующего пользователя c id:%d", s[0], s[1], userId));
+        }
     }
 }
