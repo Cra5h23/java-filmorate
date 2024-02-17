8e840c0ea30e2a9956376ccd31b0d44d3dae8f25
Nikolay <Novizdarialokin@yandex.ru>
2024-02-17 17:09:23 +0600

    feat: Добавил FilmServiceException для ошибок сервиса фильмов

--- src/main/java/ru/yandex/practicum/filmorate/exeption/LikeException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exeption;
-
-public class LikeException extends RuntimeException {
-    public LikeException(String message) {
-        super(message);
-    }
-}
