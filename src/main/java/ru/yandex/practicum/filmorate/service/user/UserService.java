6dfd5cfa2883dc6262616d82a5db607760cc5b5c
Nikolay <Novizdarialokin@yandex.ru>
2024-02-17 17:18:16 +0600

    ?

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -58,8 +58,8 @@ public class UserService {
     }
 
     public String deletingFromUserFriends(int userId, int deletingFriendId) {
-        var user = checkUser(userId,"удалить друга для");
-        var deletingFriend = checkUser(deletingFriendId,"удалить из друзей");
+        var user = checkUser(userId, "удалить друга для");
+        var deletingFriend = checkUser(deletingFriendId, "удалить из друзей");
         user.deletingFriend(deletingFriendId);
         deletingFriend.deletingFriend(userId);
         return String.format("Пользователь с id: %d удалил из друзей пользователя с id: %d", userId, deletingFriendId);
@@ -99,8 +99,8 @@ public class UserService {
     }
 
     public Collection<User> getListOfCommonFriends(int userId, int otherUserId) {
-        var user = checkUser(userId,"получить список общих друзей для");
-        var otherUser = checkUser(otherUserId,"получить список общих друзей");
+        var user = checkUser(userId, "получить список общих друзей для");
+        var otherUser = checkUser(otherUserId, "получить список общих друзей");
         return user.getFriends().stream()
                 .filter(id -> otherUser.getFriends().contains(id))
                 .map(userStorage::getUserById)
@@ -112,7 +112,6 @@ public class UserService {
 //        } catch (ValidationException e) {
 //            throw new ValidationException(format("Пользователь с id: %d, с которым вы хотите сравнить друзей не существует", otherUserId));
 //        }
-
 
 
 //        /*if (userStorage.getUserById(userId) == null) {
@@ -140,8 +139,7 @@ public class UserService {
         try {
             return userStorage.getUserById(friendId);
         } catch (UserStorageException e) {
-            throw new UserServiceException(format("Попытка %s несуществующего пользователя с id: %d",s[0], friendId));
-
+            throw new UserServiceException(format("Попытка %s несуществующего пользователя с id: %d", s[0], friendId));
 
 
 //            throw new UserServiceException(format("Попытка добавить в друзья несуществующего пользователя с id: %d", friendId));
