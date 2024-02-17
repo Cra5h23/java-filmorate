d0ed18de21c2d3f53419d4be624325215102ca60
Nikolay <Novizdarialokin@yandex.ru>
2024-02-17 17:16:02 +0600

    refactor: Перенёс проверки в отдельный метод checkUser

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -62,39 +62,75 @@ public class UserService {
 //        return String.format("Пользователь с id:%d добавил в друзья пользователя с id: %d", userId, friendId);
     }
 
-    public void deletingFromUserFriends(int userId, int deletingFriendId) {
-        if (userStorage.getUserById(userId) == null) {
-            throw new ValidationException(format("Нет пользователя с id:%d", userId));
-        }
-        if (userStorage.getUserById(deletingFriendId) == null) {
-            throw new ValidationException(format("Попытка удалить из друзей несуществующего пользователя с id:%d", deletingFriendId));
-        }
-        userStorage.getUserById(userId).getFriends().remove(deletingFriendId);
+    public String deletingFromUserFriends(int userId, int deletingFriendId) {
+        var user = checkUser(userId,"удалить друга для");
+        var deletingFriend = checkUser(deletingFriendId,"удалить из друзей");
+        user.deletingFriend(deletingFriendId);
+        deletingFriend.deletingFriend(userId);
+        return String.format("Пользователь с id: %d удалил из друзей пользователя с id: %d", userId, deletingFriendId);
+
+//        User deletingFriend;
+//        try {
+//            deletingFriend = userStorage.getUserById(deletingFriendId);
+//        } catch (ValidationException e) {
+//            throw new UserServiceException(format("Попытка удалить из друзей несуществующего пользователя с id: %d", deletingFriendId));
+//        }
+
+
+//        /*if (userStorage.getUserById(userId) == null) {
+//            throw new UserServiceException(format("Пользователь с id: %d не существует", userId));
+//        }*/
+//        if (userStorage.getUserById(deletingFriendId) == null) {
+//            throw new UserServiceException(format("Попытка удалить из друзей несуществующего пользователя с id: %d", deletingFriendId));
+//        }
+//        userStorage.getUserById(userId).getFriends().remove(deletingFriendId);
+//        userStorage.getUserById(deletingFriendId).getFriends().remove(userId);
+//        return String.format("Пользователь с id: %d удалил из друзей пользователя с id: %d", userId, deletingFriendId);
     }
 
-    public Collection<User> getAllUserFriends(int userId) {
-        if (userStorage.getUserById(userId) == null) {
-            throw new ValidationException(format("Нет пользователя с id:%d", userId));
-        }
-        return userStorage.getUserById(userId).getFriends().stream()
+    public Collection<User> getUserFriends(int userId) {
+        var user = checkUser(userId, "получить список друзей для");
+        return user.getFriends().stream()
                 .map(userStorage::getUserById)
                 .collect(Collectors.toList());
+
+
+//        /*if (userStorage.getUserById(userId) == null) {
+//            throw new ValidationException(format("Пользователь с id: %d не существует", userId));
+//        }*/
+//        return userStorage.getUserById(userId).getFriends().stream()
+//                .map(userStorage::getUserById)
+//                .collect(Collectors.toList());
     }
 
     public Collection<User> getListOfCommonFriends(int userId, int otherUserId) {
-        if (userStorage.getUserById(userId) == null) {
-            throw new ValidationException(format("Нет пользователя с id:%d", userId));
-        }
-        if (userStorage.getUserById(otherUserId) == null) {
-            throw new ValidationException(format("Пользователь с которым вы хотите сравнить друзей не существует id:%d", otherUserId));
-        }
-        /*User userById = userStorage.getUserById(userId);
-        User userById1 = userStorage.getUserById(otherUserId);
-        List<Integer> collect = userById.getFriends().stream().filter(id -> userById1.getFriends().contains(id)).collect(Collectors.toList());
-        return collect.stream().map(userStorage::getUserById).collect(Collectors.toList());*/
+        var user = checkUser(userId,"получить список общих друзей для");
+        var otherUser = checkUser(otherUserId,"получить список общих друзей");
+        return user.getFriends().stream()
+                .filter(id -> otherUser.getFriends().contains(id))
+                .map(userStorage::getUserById)
+                .collect(Collectors.toList());
 
-       return userStorage.getUserById(userId).getFriends().stream().filter(id-> userStorage.getUserById(otherUserId).getFriends().contains(id))
-                .map(userStorage::getUserById).collect(Collectors.toList());
+//        User otherUser;
+//        try {
+//            otherUser = userStorage.getUserById(otherUserId);
+//        } catch (ValidationException e) {
+//            throw new ValidationException(format("Пользователь с id: %d, с которым вы хотите сравнить друзей не существует", otherUserId));
+//        }
+
+
+
+//        /*if (userStorage.getUserById(userId) == null) {
+//            throw new ValidationException(format("Пользователь с id: %d не существует", userId));
+//        }*/
+//        if (userStorage.getUserById(otherUserId) == null) {
+//            throw new ValidationException(format("Пользователь с которым вы хотите сравнить друзей не существует id: %d", otherUserId));
+//        }
+//
+//        return userStorage.getUserById(userId).getFriends().stream()
+//                .filter(id -> userStorage.getUserById(otherUserId).getFriends().contains(id))
+//                .map(userStorage::getUserById)
+//                .collect(Collectors.toList());
     }
 
     // Методы которые должны быть
