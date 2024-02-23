34966d15de9cf606506989a088afd8327d2e864f
Nikolay <Novizdarialokin@yandex.ru>
2024-02-23 13:45:23 +0600

    refactor: Больше не используются

--- src/main/java/ru/yandex/practicum/filmorate/exeption/UserStorageException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exeption;
-
-public class UserStorageException extends RuntimeException {
-    public UserStorageException(String message) {
-        super(message);
-    }
-}
