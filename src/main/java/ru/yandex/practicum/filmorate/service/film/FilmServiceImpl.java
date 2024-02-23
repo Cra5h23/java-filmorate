95b05343360b56043a90f01276ff8ec3349d0910
Nikolay <Novizdarialokin@yandex.ru>
2024-02-23 13:44:28 +0600

    feat: Добавлен новый класс FilmServiceImpl имплементирующий новый интерфейс FilmService. Реалезует работу по добавлению удалению получению фильмов из хранилища

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmServiceImpl.java
@@ -0,0 +1,87 @@
+package ru.yandex.practicum.filmorate.service.film;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exeption.FilmServiceException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import java.util.Collection;
+
+import static java.lang.String.format;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class FilmServiceImpl implements FilmService {
+    private final FilmStorage filmStorage;
+    private int generatorFilmId = 0;
+
+    /**
+     * Метод получения списка всех фильмов
+     *
+     * @return коллекцию всех фильмов
+     */
+    @Override
+    public Collection<Film> getFilms() {
+        log.info("Запрошен список всех фильмов");
+        return filmStorage.getAllFilms();
+    }
+
+    /**
+     * @param filmId
+     * @return
+     */
+    @Override
+    public Film getFilmById(Integer filmId) {
+        log.info("Запрошен фильм с id {}", filmId);
+        return checkFilm(filmId, "получить");
+    }
+
+    /**
+     * @param film
+     * @return
+     */
+    @Override
+    public Film addFilm(Film film) {
+        film.setId(++generatorFilmId);
+        filmStorage.addFilm(film);
+        log.info("Добавлен фильм {}", film);
+        return film;
+    }
+
+    /**
+     * @param film
+     * @return
+     */
+    @Override
+    public Film updateFilm(Film film) {
+        var f = checkFilm(film.getId(), "обновить");
+
+        f.setName(film.getName());
+        f.setDescription(film.getDescription());
+        f.setReleaseDate(film.getReleaseDate());
+        f.setDuration(film.getDuration());
+        filmStorage.updateFilm(f);
+        log.info("Обновлён фильм с id: {}", film.getId());
+        return f;
+    }
+
+    /**
+     * @param filmId
+     * @return
+     */
+    @Override
+    public String deleteFilmById(Integer filmId) {
+        checkFilm(filmId, "удалить");
+        filmStorage.deleteFilm(filmId);
+        return format("Удалён фильм с id: %d", filmId);
+    }
+
+    private Film checkFilm(Integer filmId, String s) {
+        return filmStorage.getFilmById(filmId)
+                .orElseThrow(() -> new FilmServiceException(
+                        format("Попытка %s фильм с несуществующим id: %d", s, filmId)));
+    }
+}
