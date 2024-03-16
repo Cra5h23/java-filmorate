58049a48591e647f6936db2e40feb6a284b82abd
Nikolay <Novizdarialokin@yandex.ru>
2024-03-16 20:11:13 +0600

    feat: Контроллер RatingService

--- src/main/java/ru/yandex/practicum/filmorate/controller/RatingController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/RatingController.java
@@ -2,13 +2,31 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.service.rating.RatingService;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/mpa")
 public class RatingController {
+    private final RatingService ratingService;
 
+    @GetMapping
+    public ResponseEntity<?> getAllGenres() {
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ratingService.getAllRatings());
+    }
+    @GetMapping("/{genreId}")
+    public ResponseEntity<?> getGenreById(@PathVariable(required = false) Integer genreId) {
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ratingService.getRatingById(genreId));
+    }
 }
