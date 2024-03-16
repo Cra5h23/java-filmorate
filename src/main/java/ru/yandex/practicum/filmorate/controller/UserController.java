18bcee2ef6896eac308eb4df14aa4e209d78d08b
Nikolay <Novizdarialokin@yandex.ru>
2024-03-16 20:12:47 +0600

    fix: логирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,13 +2,14 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.service.user.UserService;
 import ru.yandex.practicum.filmorate.service.user.UserFriendService;
+import ru.yandex.practicum.filmorate.service.user.UserService;
 
 import javax.validation.Valid;
 import java.util.Collection;
@@ -24,10 +25,13 @@ import java.util.Collection;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @Qualifier("userFriendServiceDbImpl")
     private final UserFriendService userFriendService;
 
     @GetMapping
     public ResponseEntity<Collection<User>> getAllUsers() {
+        log.info("/GET getAllUsers");
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.APPLICATION_JSON)
@@ -36,15 +40,16 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> addNewUser(@Valid @RequestBody User user) {
+        log.info("/POST addNewUser {}", user);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(userService.addUser(user));
     }
 
-
     @PutMapping
     public ResponseEntity<User> updateUser(@Valid @RequestBody User user) {
+        log.info("PUT updateUser {}", user);
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.APPLICATION_JSON)
@@ -54,11 +59,15 @@ public class UserController {
     @PutMapping("/{userId}/friends/{friendId}")
     public ResponseEntity<?> addUserToFriends(
             @PathVariable Integer userId, @PathVariable Integer friendId) {
-        return ResponseEntity.ok(userFriendService.addingUserAsFriend(userId, friendId));
+        log.info("PUT addUserToFriends {} друга {}", userId, friendId);
+        userFriendService.addingUserAsFriend(userId, friendId);
+        return ResponseEntity.ok().build();
+        //return ResponseEntity.ok(userFriendService.addingUserAsFriend(userId, friendId));
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<?> getUserById(@PathVariable(required = false) Integer userId) {
+        log.info("GET getUserById {}", userId);
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.APPLICATION_JSON)
@@ -68,20 +77,23 @@ public class UserController {
     @DeleteMapping("/{userId}/friends/{friendId}")
     public ResponseEntity<?> deleteFromUserFriends(
             @PathVariable Integer userId, @PathVariable Integer friendId) {
+        log.info("DELETE deleteFromUserFriends user {} friend {}", userId, friendId);
         return ResponseEntity.ok(userFriendService.deletingFromUserFriends(userId, friendId));
     }
 
     @GetMapping("/{userId}/friends")
     public ResponseEntity<?> getUsersFriends(@PathVariable Integer userId) {
+        log.info("GET getUsersFriends for user id {}", userId);
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(userFriendService.getUserFriends(userId));
     }
 
-    @GetMapping("{userId}/friends/common/{otherId}")
+    @GetMapping("/{userId}/friends/common/{otherId}")
     public ResponseEntity<?> getUsersCommonFriends(
             @PathVariable Integer userId, @PathVariable Integer otherId) {
+        log.info("GET getUsersCommonFriends for user {} otherUser {}", userId, otherId);
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(userFriendService.getListOfCommonFriends(userId, otherId));
@@ -89,6 +101,7 @@ public class UserController {
 
     @DeleteMapping("/{userId}")
     public ResponseEntity<?> deleteUser(@PathVariable(required = false) int userId) {
+        log.info("DELETE deleteUser {}", userId);
         return ResponseEntity.ok(userService.deleteUserById(userId));
     }
 }
