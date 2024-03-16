b8cac480dc409b635f1d1bed5154af8b7972943f
Nikolay <Novizdarialokin@yandex.ru>
2024-03-16 17:35:44 +0600

    feat: Анотация Qualifier для использования нужной реализации

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +24,9 @@ import java.util.Collection;
 @Slf4j
 @RequiredArgsConstructor
 public class FilmController {
+    @Qualifier("filmLikeServiceDbImpl")
     private final FilmLikeService filmLikeService;
+
     private final FilmService filmService;
 
     @GetMapping
