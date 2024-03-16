35949822d2e4ce715f262c18c81d0b825b06a006
Nikolay <Novizdarialokin@yandex.ru>
2024-03-16 20:11:44 +0600

    feat: RatingServiceImpl

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/rating/impl/RatingServiceImpl.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.service.rating.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dao.RatingDao;
+import ru.yandex.practicum.filmorate.exeption.RatingServiceException;
+import ru.yandex.practicum.filmorate.model.Rating;
+import ru.yandex.practicum.filmorate.service.rating.RatingService;
+
+import java.util.Collection;
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RatingServiceImpl implements RatingService {
+    private final RatingDao ratingDao;
+    @Override
+    public Collection<Rating> getAllRatings() {
+        return (Collection<Rating>) ratingDao.findAll();
+    }
+
+    @Override
+    public Rating getRatingById(Integer ratingId) {
+       return ratingDao.findById(ratingId).orElseThrow(()-> new RatingServiceException(
+               String.format("Запрашиваемый рейтинг с id %d не существует", ratingId)));
+    }
+}
