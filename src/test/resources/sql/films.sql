494ba5b80ddbe96ed28c049cad1c8a6ad237244a
Nikolay <Novizdarialokin@yandex.ru>
2024-03-16 20:04:56 +0600

    feat: файл для тестов

---
+++ src/test/resources/sql/films.sql
@@ -0,0 +1,4 @@
+--insert into films (rating_id, name, description, release_date, duration)
+--values (1, 'testFilm1', 'testDescription1', '1989-05-02', 20);
+INSERT INTO films(name, description, release_date, duration)
+VALUES('testFilm1', 'testDescription1', '1989-05-02', 20);
