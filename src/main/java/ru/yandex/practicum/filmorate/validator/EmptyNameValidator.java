8214cbb52c90f89b3cb9321de1c6dd478737a22d
Nikolay <Novizdarialokin@yandex.ru>
2024-03-16 17:19:39 +0600

    fix: Когда имя пользователя равно null

--- src/main/java/ru/yandex/practicum/filmorate/validator/EmptyNameValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/EmptyNameValidator.java
@@ -8,7 +8,7 @@ import javax.validation.ConstraintValidatorContext;
 public class EmptyNameValidator implements ConstraintValidator<EmptyName, User> {
     @Override
     public boolean isValid(User user, ConstraintValidatorContext constraintValidatorContext) {
-        if (user.getName().isBlank() && !user.getLogin().isBlank()) {
+        if (user.getName() == null || user.getName().isBlank() && !user.getLogin().isBlank()) {
             user.setName(user.getLogin());
         }
         return true;
