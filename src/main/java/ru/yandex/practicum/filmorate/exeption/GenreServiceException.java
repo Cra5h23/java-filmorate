71312016a410cbc9657d2b3e40f2dd98d8aae488
Nikolay <Novizdarialokin@yandex.ru>
2024-03-16 21:20:49 +0600

    fix:

--- src/main/java/ru/yandex/practicum/filmorate/exeption/GenreServiceException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/GenreServiceException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exeption;
 
-public class GenreServiceException extends RuntimeException{
+public class GenreServiceException extends RuntimeException {
     public GenreServiceException(String message) {
         super(message);
     }
