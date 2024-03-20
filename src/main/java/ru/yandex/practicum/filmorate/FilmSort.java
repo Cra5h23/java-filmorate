87729dea6e0efc8cb08bbc7ee6ac2c64bd2afff8
Nikolay <Novizdarialokin@yandex.ru>
2024-03-20 21:03:10 +0600

    fix:

--- src/main/java/ru/yandex/practicum/filmorate/FilmSort.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmSort.java
@@ -12,7 +12,7 @@ public enum FilmSort {
             "f.*, \n" +
             "r.rating_name \n" +
             "FROM FILMS f\n" +
-            "LEFT JOIN ratings r on f.rating_id=r.rating_id \n"+
+            "LEFT JOIN ratings r on f.rating_id=r.rating_id \n" +
             "LEFT JOIN LIKES l ON f.FILM_ID =l.FILM_ID \n" +
             "GROUP BY f.FILM_ID \n" +
             "ORDER BY count(l.user_id) DESC\n" +
