37366fce1345de19c97bac22ac2ee3ed8f837948
Nikolay <Novizdarialokin@yandex.ru>
2024-03-20 15:12:49 +0600

    fix: удалил более ненужную реализацию

--- src/main/java/ru/yandex/practicum/filmorate/FilmSort.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmSort.java
@@ -3,11 +3,6 @@ package ru.yandex.practicum.filmorate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import ru.yandex.practicum.filmorate.model.Film;
-
-import java.util.Comparator;
-
-import static java.util.Comparator.comparingInt;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,25 +10,13 @@ import static java.util.Comparator.comparingInt;
 public enum FilmSort {
     POPULAR_FILMS_DESC("SELECT " +
             "f.*, \n" +
-            "STRING_AGG(fg.GENRE_ID, ', ') genres, \n" +
-            "STRING_AGG(l.USER_ID, ', ') likes \n" +
+            "r.rating_name \n" +
             "FROM FILMS f\n" +
-            "LEFT JOIN FILM_GENRES fg ON f.FILM_ID = fg.FILM_ID \n" +
+            "LEFT JOIN ratings r on f.rating_id=r.rating_id \n"+
             "LEFT JOIN LIKES l ON f.FILM_ID =l.FILM_ID \n" +
             "GROUP BY f.FILM_ID \n" +
             "ORDER BY count(l.user_id) DESC\n" +
-            "limit ?", comparingInt(f -> (f.getLikes().size() * -1))),
-    POPULAR_FILMS_ASC("SELECT " +
-            "f.*, \n" +
-            "STRING_AGG(fg.GENRE_ID, ', ') genres, \n" +
-            "STRING_AGG(l.USER_ID, ', ') likes \n" +
-            "FROM FILMS f\n" +
-            "LEFT JOIN FILM_GENRES fg ON f.FILM_ID = fg.FILM_ID \n" +
-            "LEFT JOIN LIKES l ON f.FILM_ID =l.FILM_ID \n" +
-            "GROUP BY f.FILM_ID \n" +
-            "ORDER BY count(l.user_id) \n" +
-            "limit ?", comparingInt(f -> (f.getLikes().size())));
+            "limit ?");
 
     private String sql;
-    private Comparator<Film> comparator;
 }
