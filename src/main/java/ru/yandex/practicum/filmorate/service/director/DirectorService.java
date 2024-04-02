71957e659cacf93b95049f1390831be1b65f40d0
Nikolay <Novizdarialokin@yandex.ru>
2024-04-02 23:32:10 +0600

    feat: Добавил метод findById

--- src/main/java/ru/yandex/practicum/filmorate/service/director/DirectorService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/director/DirectorService.java
@@ -8,6 +8,16 @@ import java.util.List;
  * @author Nikolay Radzivon
  */
 public interface DirectorService {
-
+    /**
+     * Метод получения списка всех режесёров
+     * @return список всех режесёров
+     */
     List<Director> findAll();
+
+    /**
+     * Метод получения режесёра по id
+     * @param id
+     * @return
+     */
+    Director findById(Integer id);
 }
