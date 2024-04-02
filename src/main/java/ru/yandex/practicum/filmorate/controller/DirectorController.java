73824f5d18744bec103450fdbcddb7bb309cfc0f
Nikolay <Novizdarialokin@yandex.ru>
2024-04-02 23:17:09 +0600

    feat: Реализовал эндпоинт /directors

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -1,7 +1,35 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.service.director.DirectorService;
+
+import java.util.List;
+
 /**
  * @author Nikolay Radzivon
  */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/directors")
+@Slf4j
 public class DirectorController {
+    private final DirectorService directorService;
+
+    @GetMapping
+    public ResponseEntity<List<Director>> getAllDirectors() {
+        log.info("GET /directors");
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(directorService.findAll());
+    }
+
+
 }
