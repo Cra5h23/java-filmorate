f574b0aab3b5f0700328d35637569aec8c0605d4
Nikolay <Novizdarialokin@yandex.ru>
2024-04-02 23:16:35 +0600

    feat: Добавил метод findAll

--- src/main/java/ru/yandex/practicum/filmorate/service/director/DirectorService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/director/DirectorService.java
@@ -1,4 +1,13 @@
 package ru.yandex.practicum.filmorate.service.director;
 
+import ru.yandex.practicum.filmorate.model.Director;
+
+import java.util.List;
+
+/**
+ * @author Nikolay Radzivon
+ */
 public interface DirectorService {
+
+    List<Director> findAll();
 }
