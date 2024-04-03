e836155ee0aca54a4d3248c6ea5b43ac97278a95
Nikolay <Novizdarialokin@yandex.ru>
2024-04-03 08:46:36 +0600

    feat: Добавил поле для списка режесёров

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -72,4 +72,10 @@ public class Film {
      */
     @Builder.Default
     private List<Genre> genres = new ArrayList<>();
+
+    /**
+     * Список всех режесёров
+     */
+    @Builder.Default
+    private List<Director> director = new ArrayList<>();
 }
