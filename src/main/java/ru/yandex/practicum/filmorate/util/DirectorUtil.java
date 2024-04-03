970305c1c81900ac9ddc64a75d5b9cf32da6c275
Nikolay <Novizdarialokin@yandex.ru>
2024-04-03 13:11:15 +0600

    feat: Новый ExceptionHandler для DirectorServiceException

---
+++ src/main/java/ru/yandex/practicum/filmorate/util/DirectorUtil.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.util;
+
+import ru.yandex.practicum.filmorate.model.Director;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
+/**
+ * @author Nikolqy Radzivon
+ */
+public class DirectorUtil {
+    public static Director makeDirector(ResultSet rs, int rowNum) throws SQLException {
+        return Director.builder()
+                .id(rs.getInt("director_id"))
+                .name(rs.getString("director_name"))
+                .build();
+    }
+
+    public static Map<String, Object> toMap(Director director) {
+        return Map.of("director_id", director.getId(),
+                "director_name", director.getName());
+    }
+}
