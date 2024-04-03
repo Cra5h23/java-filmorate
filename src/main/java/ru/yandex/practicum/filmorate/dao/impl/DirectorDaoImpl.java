0f0a751fee17f14b8bcc3eee09e73da64db5d0a3
Nikolay <Novizdarialokin@yandex.ru>
2024-04-03 13:13:33 +0600

    feat: Реализовал методы

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/DirectorDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/DirectorDaoImpl.java
@@ -2,15 +2,22 @@ package ru.yandex.practicum.filmorate.dao.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.dao.DirectorDao;
 import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.util.DirectorUtil;
 
 import java.util.List;
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class DirectorDaoImpl implements DirectorDao {
+    private final JdbcTemplate jdbcTemplate;
 
     /**
      * Метод получения списка всех режесёров из базы данных
@@ -19,6 +26,61 @@ public class DirectorDaoImpl implements DirectorDao {
      */
     @Override
     public List<Director> findAll() {
-        return null;
+        log.info("Получен запрос в базу данных на получение списка всех режесёров");
+        var sql = "select d.* from directors d";
+        return jdbcTemplate.query(sql, DirectorUtil::makeDirector);
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    @Override
+    public Optional<Director> findById(Integer id) {
+        var sql = "select d.* from directors d where d.director_id = ?";
+
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject(sql, DirectorUtil::makeDirector, id));
+        } catch (DataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * @param director
+     * @return
+     */
+    @Override
+    public Director save(Director director) {
+        var simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("directors")
+                .usingGeneratedKeyColumns("director_id");
+
+        director.setId(simpleJdbcInsert.executeAndReturnKey(DirectorUtil.toMap(director)).intValue());
+        log.info("Создан запрос в базу данных на добавление режесёра {}", director);
+        return director;
+    }
+
+    /**
+     * @param director
+     * @return
+     */
+    @Override
+    public Director update(Director director) {
+        var sql = "update directors set director_name = ? where director_id = ?";
+
+        jdbcTemplate.update(sql, director.getName(), director.getId());
+        return director;
+    }
+
+    /**
+     * @param id
+     */
+    @Override
+    public void delete(Integer id) {
+        var sql = "delete films_directors where director_id = ?;" +
+                "delete directors where director_id = ?;";
+
+        jdbcTemplate.update(sql, id, id);
     }
 }
