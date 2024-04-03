d302d95197e5f205bf986ebc5ab62cdaa6bf40ac
Nikolay <Novizdarialokin@yandex.ru>
2024-04-03 08:45:53 +0600

    feat: Добавил поля id, name

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -1,7 +1,19 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
 /**
  * @author Nikolay Radzivon
  */
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Getter
+@Setter
+@AllArgsConstructor
 public class Director {
+    private Integer id;
+    @NotBlank(message = "Имя режесёра не должно быть пустым")
+    private String name;
 }
