49d88179f4dfd2f2e2bf299abf9e10abd0421ddb
Rakhmanov Ayrat <prtnr.ayrat.rakhmanov@rtmis.ru>
2024-04-05 15:48:32 +0500

    Разработка функционала обзоры фильмов.
    Доработка скрипов создания таблиц reviews и reviewratings.
    Удалил не используемое исключение ReviewServiceException.java

--- src/main/java/ru/yandex/practicum/filmorate/exeption/ReviewServiceException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exeption;
-
-public class ReviewServiceException extends RuntimeException {
-    public ReviewServiceException(String message) {
-        super(message);
-    }
-}
